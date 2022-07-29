dc9890f296c27d09f24de86a84ca708f3a61b46c
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2022-07-29 11:48:47 +0200

    Add package

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+﻿export { AliasHelper } from './helpers/AliasHelper';
+
+export * from './models/dataTypes';
+export * from './models/properties';
+export { Template } from './models/template';
+export { Stylesheet } from './models/stylesheet';
+export { CmsDocumentType } from './models/cmsDocumentType';
