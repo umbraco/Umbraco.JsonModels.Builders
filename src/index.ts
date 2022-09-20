7367a6701aa71eec690c1cb249a1b9346515d305
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2022-09-20 13:56:17 +0200

    Export builder folder

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,6 @@ export * from './models/properties';
 export { Template } from './models/template';
 export { Stylesheet } from './models/stylesheet';
 export { CmsDocumentType } from './models/cmsDocumentType';
-export { PartialViewMacro } from './models/partialViewMacros/partialViewMacro'
-export { Package } from './models/package'
+export { PartialViewMacro } from './models/partialViewMacros/partialViewMacro';
+export { Package } from './models/package';
+export * from './builders';
