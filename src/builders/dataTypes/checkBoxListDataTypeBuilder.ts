7d0d3d7d9ef729b129357b00b9353b4403b334aa
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2022-09-20 13:14:13 +0200

    Add builders to Models packageBuilder.ts

---
+++ src/builders/dataTypes/checkBoxListDataTypeBuilder.ts
@@ -0,0 +1,12 @@
+import { DataTypeBuilder } from './dataTypeBuilder';
+import { CheckBoxListDataType } from '../../models';
+
+export class CheckBoxListDataTypeBuilder extends DataTypeBuilder {
+  constructor(private checkBoxListDataType: CheckBoxListDataType = new CheckBoxListDataType()) {
+    super(checkBoxListDataType);
+  }
+  withPrevalues(value: string[], multiSelect = false) {
+    this.checkBoxListDataType.addPrevalues(value, multiSelect);
+    return this;
+  }
+}
