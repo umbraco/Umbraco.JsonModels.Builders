4deb0682a25be18d6c7934d754b1c8ff32cb166b
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2022-09-20 14:48:29 +0200

    Fix up gitignore

--- src/models/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './dataTypes';
-export * from './properties';
-export { Template } from './template';
-export { Stylesheet } from './stylesheet';
-export { CmsDocumentType } from './cmsDocumentType';
-export { PartialViewMacro } from './partialViewMacros/partialViewMacro'
-export { Package } from './package'
