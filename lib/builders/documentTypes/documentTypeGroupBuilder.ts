1b1729247b36e3166d2e1c41317c1c25a94e2e23
Andreas Zerbst <andr317c@live.dk>
2023-02-14 11:31:46 +0100

    Added AddImagePickerProperty

--- lib/builders/documentTypes/documentTypeGroupBuilder.ts
+++ lib/builders/documentTypes/documentTypeGroupBuilder.ts
@@ -4,7 +4,7 @@ import { FormPickerDocumentTypePropertyBuilder } from './properties/formPickerDo
 import { TextBoxDocumentTypePropertyBuilder } from './properties/textBoxDocumentTypePropertyBuilder';
 import { DropDownDocumentTypePropertyBuilder } from './properties/dropDownDocumentTypePropertyBuilder';
 import { ContentPickerPropertyBuilder } from './properties/contentPickerTypePropertyBuilder';
-import { RichTextDocumentTypePropertyEditor, CustomDocumentTypePropertyBuilder } from './properties';
+import {RichTextDocumentTypePropertyEditor, CustomDocumentTypePropertyBuilder, MediaPickerDocumentTypePropertyBuilder} from './properties';
 import { UrlPickerPropertyBuilder } from './properties/urlPickerTypePropertyBuilder';
 
 
@@ -74,6 +74,12 @@ export default class DocumentTypeGroupBuilder {
     return builder;
   }
 
+  addImagePickerProperty() {
+    const builder = new MediaPickerDocumentTypePropertyBuilder(this);
+    this.documentTypeGroupPropertyBuilders.push(builder);
+    return builder;
+  }
+  
   addCustomProperty(id) {
     const builder = new CustomDocumentTypePropertyBuilder(this, id);
     this.documentTypeGroupPropertyBuilders.push(builder);
