d80ff09fcfe479642b56402d4e4c9f15844ec95a
Andreas Zerbst <andr317c@live.dk>
2023-02-21 08:18:53 +0100

    Added ImageBuilder

---
+++ lib/builders/content/blockGridProperties/blockGridImageBuilder.ts
@@ -0,0 +1,52 @@
+﻿import {BlockGridEntryBuilder} from "./blockGridEntryBuilder";
+
+export class BlockGridImageBuilder {
+
+  parentBuilder;
+  mediaKey;
+  crops;
+  left;
+  top;
+
+  constructor(parentBuilder: BlockGridEntryBuilder) {
+    this.crops = [];
+    this.parentBuilder = parentBuilder;
+  }
+
+  withMediaKey(mediaKey: string) {
+    this.mediaKey = mediaKey;
+    return this;
+  }
+
+  withLeftFocalPoint(leftFocalPoint: number) {
+    this.left = leftFocalPoint;
+    return this;
+  }
+
+  withTopFocalPoint(topFocalPoint: number) {
+    this.top = topFocalPoint;
+    return this
+  }
+
+  done() {
+    return this.parentBuilder;
+  }
+
+  generateUdi() {
+    const crypto = require('crypto');
+    return crypto.randomUUID();
+  }
+  
+  build() {
+
+    return {
+      key: this.generateUdi() || null,
+      mediaKey: this.mediaKey || null,
+      crops: [],
+      focalPoint: {
+        left: this.left || 0.5,
+        top: this.top || 0.5
+      },
+    }
+  }
+}
