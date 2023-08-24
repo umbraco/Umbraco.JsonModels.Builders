e4fdf79fba5a5ab34eb1b4e95dc5c990784ff8c3
Andreas Zerbst <andr317c@live.dk>
2023-08-24 14:59:03 +0200

    Added packageBuilder

---
+++ lib/builders/packages/packageBuilder.ts
@@ -0,0 +1,105 @@
+﻿export class PackageBuilder {
+  name: string;
+  contentNodeId: string;
+  contentLoadChildNodes: boolean;
+  mediaIds: string[];
+  mediaLoadChildNodes: boolean;
+  documentTypes: string[];
+  mediaTypes: string[];
+  dataTypes: string[];
+  templates: string[];
+  partialViews: string[]
+  stylesheets: string[]
+  scripts: string[];
+  languages: string[]
+  dictionaryItems: string[];
+
+  withName(name: string) {
+    this.name = name;
+    return this;
+  }
+
+  withContentNodeId(contentNodeId: string) {
+    this.contentNodeId = contentNodeId;
+    return this;
+  }
+
+  withContentLoadChildNodes(contentLoadChildNodes: boolean) {
+    this.contentLoadChildNodes = contentLoadChildNodes;
+    return this;
+  }
+
+  withMediaIds(mediaIds: string[]) {
+    this.mediaIds = mediaIds;
+    return this;
+  }
+
+  withMediaLoadChildNodes(mediaLoadChildNodes: boolean) {
+    this.mediaLoadChildNodes = mediaLoadChildNodes;
+    return this;
+  }
+
+  withDocumentTypes(documentTypes: string[]) {
+    this.documentTypes = documentTypes;
+    return this;
+  }
+
+  withMediaTypes(mediaTypes: string[]) {
+    this.mediaTypes = mediaTypes;
+    return this;
+  }
+
+  withDataTypes(dataTypes: string[]) {
+    this.dataTypes = dataTypes
+    return this;
+  }
+
+  withTemplates(templates: string[]) {
+    this.templates = templates;
+    return this;
+  }
+
+  withPartialViews(partialViews: string[]) {
+    this.partialViews = partialViews;
+    return this;
+  }
+
+  withStylesheets(stylesheets: string[]) {
+    this.stylesheets = stylesheets;
+    return this;
+  }
+
+  withScripts(scripts: string[]) {
+    this.scripts = scripts;
+    return this;
+  }
+
+  withLanguages(languages: string[]) {
+    this.languages = languages;
+    return this;
+  }
+
+  withDictionaryItems(dictionaryItems: string[]) {
+    this.dictionaryItems = dictionaryItems;
+    return this;
+  }
+
+  build() {
+    return {
+      name: this.name,
+      contentNodeId: this.contentNodeId || "",
+      contentLoadChildNodes: this.contentLoadChildNodes || true,
+      mediaIds: this.mediaIds,
+      mediaLoadChildNodes: this.mediaLoadChildNodes || true,
+      documentTypes: this.documentTypes,
+      mediaTypes: this.mediaTypes,
+      dataTypes: this.dataTypes,
+      templates: this.templates,
+      partialViews: this.partialViews,
+      stylesheets: this.stylesheets,
+      scripts: this.scripts,
+      languages: this.languages,
+      dictionaryItems: this.dictionaryItems,
+    };
+  }
+}
