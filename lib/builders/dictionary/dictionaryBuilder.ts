dadaffc7239ad5727bed0133762e2dbfae69f3d8
Andreas Zerbst <andr317c@live.dk>
2023-08-25 15:23:17 +0200

    Created a dictionaryBuilder.ts

---
+++ lib/builders/dictionary/dictionaryBuilder.ts
@@ -0,0 +1,37 @@
+﻿import {DictionaryTranslationBuilder} from "./dictionaryTranslationBuilder";
+
+export class DictionaryBuilder {
+  name: string;
+  parentId: string;
+  dictionaryTranslationBuilder;
+
+  constructor() {
+    this.dictionaryTranslationBuilder = [];
+  }
+
+  withName(name: string) {
+    this.name = name;
+    return this;
+  }
+
+  addTranslation() {
+    const builder = new DictionaryTranslationBuilder(this);
+    this.dictionaryTranslationBuilder.push(builder);
+    return builder;
+  }
+  
+  withParentId(parentId: string) {
+    this.parentId = parentId;
+    return this;
+  }
+
+  build() {
+    return {
+      name: this.name || undefined,
+      translations: this.dictionaryTranslationBuilder.map(builder => {
+        return builder.build();
+      }),
+      parentId: this.parentId || undefined
+    }
+  }
+}
