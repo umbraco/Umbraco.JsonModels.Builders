aa29145b675dda069f6574cef7262cbd326e79cb
Andreas Zerbst <andr317c@live.dk>
2023-08-25 10:48:24 +0200

    Created builder for SliderDataTypes

---
+++ lib/builders/dataTypes/sliderDataTypeBuilder.ts
@@ -0,0 +1,77 @@
+﻿import {DataTypeBuilder} from "./dataTypeBuilder";
+
+export class SliderDataTypeBuilder extends DataTypeBuilder {
+  minVal: number;
+  maxVal: number;
+  enableRange: boolean;
+  initVal1: number;
+  initVal2: number;
+  step: number;
+
+  constructor() {
+    super();
+    this.propertyEditorAlias = "Umbraco.Slider";
+    this.propertyEditorUiAlias = "Umb.PropertyEditorUi.Slider";
+  }
+
+  withMinValue(minValue: number) {
+    this.minVal = minValue;
+    return this;
+  }
+
+  withMaxValue(maxValue: number) {
+    this.maxVal = maxValue;
+    return this;
+  }
+
+  toEnableRange(enableRange: boolean) {
+    this.enableRange = enableRange;
+    return this;
+  }
+
+  withInitialValueOne(initialValueOne: number) {
+    this.initVal1 = initialValueOne;
+    return this;
+  }
+
+  withInitialValueTwo(initialValueTwo: number) {
+    if (this.enableRange) {
+      this.initVal2 = initialValueTwo;
+    }
+    return this;
+  }
+
+  withStep(step: number) {
+    this.step = step;
+    return this;
+  }
+
+  getValues() {
+    let values: any = [];
+    values.push({
+      alias: 'minVal',
+      value: this.minVal || 0,
+    });
+    values.push({
+      alias: 'maxVal',
+      value: this.maxVal || 0,
+    });
+    values.push({
+      alias: 'enableRange',
+      value: this.enableRange || false,
+    });
+    values.push({
+      alias: 'initVal1',
+      value: this.initVal1 || 0,
+    });
+    values.push({
+      alias: 'initVal2',
+      value: this.initVal2 || 0,
+    });
+    values.push({
+      alias: 'step',
+      value: this.step || 0,
+    });
+    return values;
+  }
+}
