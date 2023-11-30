b0cbcc342fc5ecff49710d94098ed6abfe2fd2a7
Andreas Zerbst <andr317c@live.dk>
2023-11-30 08:54:27 +0100

    Builders for webhook

---
+++ lib/builders/webhooks/webhookBuilder.ts
@@ -0,0 +1,75 @@
+﻿import {WebhookEventBuilder} from "./webhookEventBuilder";
+import {WebhookHeaderBuilder} from "./webhookHeaderBuilder";
+
+export class WebhookBuilder {
+    key: string;
+    url: string;
+    contentTypeKeys;
+    enabled: boolean;
+    name: string;
+    webhookEventBuilder;
+    webhookHeaderBuilder;
+
+    constructor() {
+        this.contentTypeKeys = [];
+        this.webhookEventBuilder = [];
+        this.webhookHeaderBuilder = [];
+    }
+
+    withKey(key: string) {
+        this.key = key;
+        return this;
+    };
+
+    withName(name: string) {
+        this.name = name;
+        return this;
+    }
+
+    isEnabled(enabled: boolean) {
+        this.enabled = enabled;
+        return this;
+    }
+
+    addEvent() {
+        const builder = new WebhookEventBuilder(this);
+        this.webhookEventBuilder.push(builder);
+        return builder;
+    }
+
+    addContentTypeKey(contentTypeKey: string) {
+        this.contentTypeKeys.push(contentTypeKey);
+        return this;
+    }
+
+    addHeader() {
+        const builder = new WebhookHeaderBuilder(this);
+        this.webhookHeaderBuilder.push(builder);
+        return builder;
+    }
+
+    withUrl(url) {
+        this.url = url;
+        return this;
+    }
+
+    build() {
+        const headers = {};
+        this.webhookHeaderBuilder.forEach((builder) => {
+            const header = builder.build();
+            Object.assign(headers, header);
+        });
+
+        return {
+            key: this.key || "",
+            url: this.url || "",
+            events: this.webhookEventBuilder.map((builder) => {
+                return builder.build();
+            }),
+            contentTypeKeys: this.contentTypeKeys || [],
+            enabled: this.enabled || true,
+            headers: headers || {},
+            name: this.name || "",
+        };
+    }
+}
---
+++ lib/builders/webhooks/webhookEventBuilder.ts
@@ -0,0 +1,51 @@
+﻿export class WebhookEventBuilder {
+    parentBuilder;
+    eventName;
+    eventType: string;
+    alias: string;
+
+
+    constructor(parentBuilder) {
+        this.parentBuilder = parentBuilder;
+    }
+
+    withEventName(eventName: string) {
+        this.eventName = eventName;
+        return this;
+    }
+
+    done() {
+        return this.parentBuilder;
+    }
+
+    build() {
+        switch (this.eventName) {
+            case "Content was deleted":
+                this.alias = "Umbraco.ContentDelete";
+                this.eventType = "Content";
+                break;
+            case "Content was published":
+                this.alias = "Umbraco.ContentPublish";
+                this.eventType = "Content";
+                break;
+            case "Content was unpublished":
+                this.alias = "Umbraco.ContentUnpublish";
+                this.eventType = "Content";
+                break;
+            case "Media was deleted":
+                this.alias = "Umbraco.MediaDelete";
+                this.eventType = "Media";
+                break;
+            case "Media was saved":
+                this.alias = "Umbraco.MediaSave";
+                this.eventType = "Media";
+                break;
+        }
+
+        return {
+            eventName: this.eventName,
+            eventType: this.eventType,
+            alias: this.alias,
+        };
+    }
+}
