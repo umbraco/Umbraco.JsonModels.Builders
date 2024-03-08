a6775adcddf17a093969a473ac0f1c4453092c50
Nhu Dinh <hnd@umbraco.dk>
2024-03-08 14:47:35 +0700

    Updated documentType

--- lib/builders/document/documentBuilder.ts
+++ lib/builders/document/documentBuilder.ts
@@ -61,7 +61,7 @@ export class DocumentBuilder {
       }),
       id: this.id,
       parent: this.parentId ? { id: this.parentId} : null,
-      documentType: this.documentTypeId ? { id: this.documentTypeId},
+      documentType: { id: this.documentTypeId},
       template: this.templateId ? { id: this.templateId} : null,
     };
   }
