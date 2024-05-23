22fc731075912b35adfe5af0830009c13f43b1d5
Andreas Zerbst <andr317c@live.dk>
2024-05-23 09:49:04 +0200

    Added v2/blockListBuilder

---
+++ lib/builders/dataTypes/blockListBuilder/blockListBlockBuilder.ts
@@ -0,0 +1,117 @@
+﻿import {BlockListDataTypeBuilder} from "../blockListDataTypeBuilder";
+
+export enum EditorSize {
+  Small = "Small",
+  Medium = "Medium",
+  Large = "Large",
+  Full = "Full"
+}
+
+export class BlockListBlockBuilder {
+  parentBuilder: BlockListDataTypeBuilder;
+  contentElementTypeKey: string;
+  label: string;
+  editorSize : EditorSize;
+  settingsElementTypeKey: string;
+  backgroundColor: string;
+  iconColor: string;
+  stylesheet: string[];
+
+  constructor(parentBuilder: BlockListDataTypeBuilder) {
+    this.parentBuilder = parentBuilder;
+    this.stylesheet = [];
+  }
+
+  withContentElementTypeKey(contentElementTypeKey: string) {
+    this.contentElementTypeKey = contentElementTypeKey;
+    return this;
+  }
+
+  withLabel(label: string) {
+    this.label = label;
+    return this;
+  }
+
+  withEditorSize(editorSize: EditorSize) {
+    this.editorSize = editorSize;
+    return this;
+  }
+
+  withSettingsElementTypeKey(settingsElementTypeKey: string) {
+    this.settingsElementTypeKey = settingsElementTypeKey;
+    return this;
+  }
+
+  withBackgroundColor(backgroundColor: string) {
+    this.backgroundColor = backgroundColor;
+    return this;
+  }
+
+  withIconColor(iconColor: string) {
+    this.iconColor = iconColor;
+    return this;
+  }
+
+  withStylesheet(stylesheet: string) {
+    this.stylesheet.push(stylesheet);
+    return this;
+  }
+
+  done() {
+    return this.parentBuilder;
+  }
+
+  getValues() {
+    let values: any = [];
+
+      values.push({
+        alias: "contentElementTypeKey",
+        value: this.contentElementTypeKey || ""
+      });
+
+    if (this.label) {
+      values.push({
+        alias: "label",
+        value: this.label
+      });
+    }
+
+    if (this.editorSize) {
+      values.push({
+        alias: "editorSize",
+        value: this.editorSize
+      });
+    }
+
+    if (this.settingsElementTypeKey) {
+      values.push({
+        alias: "settingsElementTypeKey",
+        value: this.settingsElementTypeKey
+      });
+    }
+
+    if (this.backgroundColor) {
+      values.push({
+        alias: "backgroundColor",
+        value: this.backgroundColor
+      });
+    }
+
+    if (this.iconColor) {
+      values.push({
+        alias: "iconColor",
+        value: this.iconColor
+      });
+    }
+
+    if (this.stylesheet.length > 0) {
+      values.push({
+        alias: "stylesheet",
+        value: this.stylesheet
+      });
+    }
+
+    return values;
+  }
+
+}
---
+++ lib/builders/dataTypes/blockListDataTypeBuilder.ts
@@ -0,0 +1,92 @@
+﻿import {DataTypeBuilder} from "./dataTypeBuilder";
+import {BlockListBlockBuilder} from "./blockListBuilder";
+
+export class BlockListDataTypeBuilder extends DataTypeBuilder {
+  minValue: number;
+  maxValue: number;
+  maxPropertyWidth: number;
+  useSingleBlockMode: boolean;
+  useLiveEditing: boolean;
+  useInlineEditingAsDefault: boolean;
+  blockListBlockBuilder: BlockListBlockBuilder[];
+
+  constructor() {
+    super();
+    this.blockListBlockBuilder = [];
+    this.editorAlias = "Umbraco.BlockList";
+    this.editorUiAlias = "Umb.PropertyEditorUi.BlockList";
+  }
+
+  withMinValue(minValue: number) {
+    this.minValue = minValue;
+    return this;
+  }
+
+  withMaxValue(maxValue: number) {
+    this.maxValue = maxValue;
+    return this;
+  }
+
+  withMaxPropertyWidth(maxPropertyWidth: number) {
+    this.maxPropertyWidth = maxPropertyWidth;
+    return this;
+  }
+
+  withSingleBlockMode(useSingleBlockMode: boolean) {
+    this.useSingleBlockMode = useSingleBlockMode;
+    return this;
+  }
+
+  withLiveEditing(useLiveEditing: boolean) {
+    this.useLiveEditing = useLiveEditing;
+    return this;
+  }
+
+  withInlineEditingAsDefault(useInlineEditingAsDefault: boolean) {
+    this.useInlineEditingAsDefault = useInlineEditingAsDefault;
+    return this;
+  }
+
+  addBlock(blockListBlockBuilder: BlockListBlockBuilder) {
+    this.blockListBlockBuilder.push(blockListBlockBuilder);
+    return this;
+  }
+
+  getValues() {
+    let values: any = [];
+
+    if (this.minValue > this.maxValue) {
+      this.minValue = this.maxValue;
+    }
+
+    values.push({
+      alias: "validationLimit",
+      value: {
+        min: this.minValue || "",
+        max: this.maxValue || ""
+      }
+    });
+    values.push({
+      alias: "maxPropertyWidth",
+      value: this.maxPropertyWidth || ""
+    });
+    values.push({
+      alias: "useSingleBlockMode",
+      value: this.useSingleBlockMode || false
+    });
+    values.push({
+      alias: "useLiveEditing",
+      value: this.useLiveEditing || false
+    });
+    values.push({
+      alias: "useInlineEditingAsDefault",
+      value: this.useInlineEditingAsDefault || false
+    });
+    values.push({
+      alias: "blocks",
+      value: this.blockListBlockBuilder.map(block => block.getValues()) || null
+    });
+
+    return values;
+  }
+}
