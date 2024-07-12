d977a0de45f9ab794ac1d6c4afb4deed95f665e2
Nhu Dinh <hnd@umbraco.dk>
2024-07-12 13:27:26 +0700

    Removed offsetTime attribute due to api changes

--- lib/builders/dataTypes/datePickerDataTypeBuilder.ts
+++ lib/builders/dataTypes/datePickerDataTypeBuilder.ts
@@ -1,18 +1,12 @@
 ﻿import {DataTypeBuilder} from "./dataTypeBuilder";
 
 export class DatePickerDataTypeBuilder extends DataTypeBuilder {
-  offsetTime: boolean;
   format: string;
 
   constructor() {
     super();
     this.editorAlias = "Umbraco.DateTime";
     this.editorUiAlias = "Umb.PropertyEditorUi.DatePicker";
-  }
-
-  withOffsetTime(offsetTime: boolean) {
-    this.offsetTime = offsetTime;
-    return this;
   }
 
   withFormat(format: string) {
@@ -22,10 +16,6 @@ export class DatePickerDataTypeBuilder extends DataTypeBuilder {
 
   getValues() {
     let values: any = [];
-    values.push({
-      alias: "offsetTime",
-      value: this.offsetTime || false
-    });
     values.push({
       alias: "format",
       value: this.format || "YYYY-MM-DD"
