d4b3e8fc21dc2c3f3fc084ab03fb546940686f09
Nhu Dinh <hnd@umbraco.dk>
2024-08-08 22:14:38 +0700

    Added json builder for radiobox data type

---
+++ lib/builders/dataTypes/radioboxDataTypeBuilder.ts
@@ -0,0 +1,28 @@
+﻿import {DataTypeBuilder} from "./dataTypeBuilder";
+
+export class RadioboxDataTypeBuilder extends DataTypeBuilder {
+  items: string[];
+
+  constructor() {
+    super();
+    this.editorAlias = "Umbraco.RadioButtonList";
+    this.editorUiAlias = "Umb.PropertyEditorUi.RadioButtonList";
+  }
+
+  withItems(values: string[]) {
+    this.items = values;
+    return this;
+  }
+
+  getValues() {
+    let values: any[] = [];
+
+    if (this.items && this.items.length > 0) {
+      values.push({
+        alias: "items",
+        value: this.items
+      });
+    }
+    return values;
+  }
+}
