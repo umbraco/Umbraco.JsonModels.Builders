bc3067a633d0fb1c3dc85b1753d34ff8c6318061
Andreas Zerbst <andr317c@live.dk>
2024-08-08 14:00:00 +0200

    Created builder

---
+++ lib/builders/dataTypes/LabelDataTypeBuilder.ts
@@ -0,0 +1,25 @@
+﻿import {DataTypeBuilder} from "./dataTypeBuilder";
+
+export class LabelDataTypeBuilder extends DataTypeBuilder {
+  value: string;
+
+  constructor() {
+    super();
+    this.editorAlias = "Umbraco.Label";
+    this.editorUiAlias = "Umb.PropertyEditorUi.Label";
+  }
+
+  withValue(value: string) {
+    this.value = value;
+    return this;
+  }
+
+  getValues() {
+    let values: any = [];
+    values.push({
+      alias: "umbracoDataValueType",
+      value: this.value || "STRING"
+    });
+    return values;
+  }
+}
