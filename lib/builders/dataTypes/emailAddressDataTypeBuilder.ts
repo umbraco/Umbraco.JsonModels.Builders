a5a6793cf76cdad9ca4aefb7a24d6d6885e84163
Nhu Dinh <hnd@umbraco.dk>
2024-08-29 16:13:28 +0700

    Added json builder for email address data type

---
+++ lib/builders/dataTypes/emailAddressDataTypeBuilder.ts
@@ -0,0 +1,25 @@
+﻿import {DataTypeBuilder} from "./dataTypeBuilder";
+
+export class EmailAddressDataTypeBuilder extends DataTypeBuilder {
+  inputType: string;
+
+  constructor() {
+    super();
+    this.editorAlias = "Umbraco.EmailAddress";
+    this.editorUiAlias = "Umb.PropertyEditorUi.EmailAddress";
+  }
+
+  withInputType(inputType: string) {
+    this.inputType = inputType;
+    return this;
+  }
+
+  getValues() {
+    let values: any = [];
+    values.push({
+      alias: 'inputType',
+      value: this.inputType || 'email'
+    });
+    return values;
+  }
+}
