975254e32d4037b98fe6bf86a53138e1921a6ac8
Nhu Dinh <hnd@umbraco.dk>
2024-09-04 09:51:27 +0700

    Removed withInputType method

--- lib/builders/dataTypes/emailAddressDataTypeBuilder.ts
+++ lib/builders/dataTypes/emailAddressDataTypeBuilder.ts
@@ -1,24 +1,17 @@
 ﻿import {DataTypeBuilder} from "./dataTypeBuilder";
 
 export class EmailAddressDataTypeBuilder extends DataTypeBuilder {
-  inputType: string;
-
   constructor() {
     super();
     this.editorAlias = "Umbraco.EmailAddress";
     this.editorUiAlias = "Umb.PropertyEditorUi.EmailAddress";
   }
 
-  withInputType(inputType: string) {
-    this.inputType = inputType;
-    return this;
-  }
-
   getValues() {
     let values: any = [];
     values.push({
       alias: 'inputType',
-      value: this.inputType || 'email'
+      value: 'email'
     });
     return values;
   }
