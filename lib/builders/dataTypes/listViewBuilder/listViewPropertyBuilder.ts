27ccd2919d20dfbdecd9e007f9db2e2011b3918b
Andreas Zerbst <andr317c@live.dk>
2024-09-17 13:55:13 +0200

    Added builder for list view

---
+++ lib/builders/dataTypes/listViewBuilder/listViewPropertyBuilder.ts
@@ -0,0 +1,38 @@
+﻿import {ListViewDataTypeBuilder} from "../listViewDataTypeBuilder";
+
+export class ListViewPropertyBuilder {
+  parentBuilder: ListViewDataTypeBuilder;
+  alias: string;
+  header: string;
+  nameTemplate: string;
+
+  constructor(parentBuilder: ListViewDataTypeBuilder) {
+    this.parentBuilder = parentBuilder;
+  }
+  
+  withAlias(alias: string) {
+    this.alias = alias;
+    return this;
+  }
+  
+  withHeader(header: string) {
+    this.header = header;
+    return this;
+  }
+  
+  withNameTemplate(nameTemplate: string) {
+    this.nameTemplate = nameTemplate;
+    return this;
+  }
+  done() {
+    return this.parentBuilder;
+  }
+
+  getValues() {
+    return {
+      alias: this.alias || "sortOrder",
+      header: this.header || "Sort",
+      nameTemplate: this.nameTemplate || null
+    };
+  }
+}
