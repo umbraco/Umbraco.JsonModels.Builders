14db25b68bf9882d14b23ccc4b1bbd11ed0844f5
Nhu Dinh <150406148+nhudinh0309@users.noreply.github.com>
2025-01-14 10:16:46 +0700

    V2 Updated json builder for document with block list and block grid (#52)
    * Added json builder for block grid value in content
    
    * Added json builder for block grid value in content
    
    * Updated json builder for document with block list and block grid
    
    * Bumped version
    
    * Fixed comments
    
    * Cleaned up

---
+++ lib/builders/document/blockGridValueBuilder/blockGridLayoutBuilder.ts
@@ -0,0 +1,62 @@
+﻿import {BlockGridValueBuilder} from "./blockGridValueBuilder";
+
+export class BlockGridLayoutBuilder {
+  parentBuilder: BlockGridValueBuilder;
+  columnSpan: number;
+  contentKey: string;
+  contentUdi: string;
+  rowSpan: number;
+  settingsKey: string;
+  settingsUdi: string;
+
+  constructor(parentBuilder: BlockGridValueBuilder) {
+    this.parentBuilder = parentBuilder;
+  }
+
+  withColumnSpan(columnSpan: number) {
+    this.columnSpan = columnSpan;
+    return this;
+  }
+
+  withContentKey(contentKey: string) {
+    this.contentKey = contentKey;
+    return this;
+  }
+
+  withContentUdi(contentUdi: string) {
+    this.contentUdi = contentUdi;
+    return this;
+  }
+
+  withRowSpan(rowSpan: number) {
+    this.rowSpan = rowSpan;
+    return this;
+  }
+
+  withSettingsUdi(settingsUdi: string) {
+    this.settingsUdi = settingsUdi;
+    return this;
+  }
+
+  withSettingsKey(settingsKey: string) {
+    this.settingsKey = settingsKey;
+    return this;
+  }
+
+  done() {
+    return this.parentBuilder;
+  }
+
+  getValue() {
+    return {
+      $type: 'BlockGridLayoutItem',
+      areas: [],
+      columnSpan: this.columnSpan || 12,
+      contentKey: this.contentKey,
+      contentUdi: this.contentUdi || null,
+      rowSpan: this.rowSpan || 1,
+      settingsKey: this.settingsKey || null,
+      settingsUdi: this.settingsUdi || null
+    };
+  }
+}
---
+++ lib/builders/document/blockGridValueBuilder/index.ts
@@ -0,0 +1,5 @@
+﻿export {BlockGridValueBuilder} from './blockGridValueBuilder'
+export {BlockGridLayoutBuilder} from './blockGridLayoutBuilder'
+export {BlockGridContentDataBuilder} from './blockGridContentDataBuilder';
+export {BlockGridExposeBuilder} from './blockGridExposeBuilder';
+export {BlockGridContentDataValueBuilder} from './blockGridContentDataValueBuilder';
