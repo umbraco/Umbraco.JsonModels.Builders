9678e33dd7bd298f0dacf747eab7a811df642297
Nhu Dinh <150406148+nhudinh0309@users.noreply.github.com>
2025-02-19 17:10:50 +0700

    V2 Added json builder for document domain (#58)
    * Added json builder for document domain value
    
    * Added json builder for document domain
    
    * Bumped version
    
    * Reverted

--- lib/builders/document/index.ts
+++ lib/builders/document/index.ts
@@ -1,3 +1,4 @@
 ﻿export {DocumentBuilder} from './documentBuilder';
 export {DocumentVariantBuilder} from './documentVariantBuilder';
 export {DocumentValueBuilder} from './documentValueBuilder';
+export {DocumentDomainBuilder} from './documentDomainBuilder/documentDomainBuilder';
