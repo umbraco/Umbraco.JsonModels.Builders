ccd79832a30c3819420dc7014d7c2f6c249c5fac
Andreas Zerbst <andr317c@live.dk>
2025-11-04 13:52:58 +0100

    Added min max

--- lib/builders/dataTypes/entityDataPickerDataTypeBuilder.ts
+++ lib/builders/dataTypes/entityDataPickerDataTypeBuilder.ts
@@ -1,12 +1,24 @@
 ﻿import {DataTypeBuilder} from "./dataTypeBuilder";
 
 export class EntityDataPickerDataTypeBuilder extends DataTypeBuilder {
+  minValue: number;
+  maxValue: number;
   dataSource: string;
 
   constructor() {
     super();
     this.editorAlias = "Umbraco.EntityDataPicker";
     this.editorUiAlias = "Umb.PropertyEditorUi.EntityDataPicker";
+  }
+
+  withMinValue(minValue: number) {
+    this.minValue = minValue;
+    return this;
+  }
+
+  withMaxValue(maxValue: number) {
+    this.maxValue = maxValue;
+    return this;
   }
 
   withDataSource(dataSource: string) {
@@ -16,6 +28,18 @@ export class EntityDataPickerDataTypeBuilder extends DataTypeBuilder {
 
   getValues() {
     let values: any = [];
+
+    // Add validationLimit alias and value if present
+    if (this.minValue !== undefined || this.maxValue !== undefined) {
+      values.push({
+        alias: "validationLimit",
+        value: {
+          min: this.minValue !== undefined ? this.minValue : "",
+          max: this.maxValue !== undefined ? this.maxValue : ""
+        }
+      });
+    }
+
     values.push({
       alias: 'umbEditorDataSource',
       value: this.dataSource
